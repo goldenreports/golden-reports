a196ec456adfd0dfd0f98490f82f103e1eff6294
Mariano Santoro <santoromariano88@gmail.com>
2023-01-24 00:11:49 -0600

    Add data source form

--- GoldenReports.UI/src/app/features/data-sources/data-sources.module.ts
+++ GoldenReports.UI/src/app/features/data-sources/data-sources.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { DataSourceEditorPageEffects } from './store/data-source-editor-page';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 import { SharedModule } from '@shared';
 import { DataSourcesRoutingModule } from './data-sources-routing.module';
 import { DataSourceEditorComponent, DataSourceListComponent } from './pages';
 import { dataSourceFeatureReducer, dataSourceFeatureStateKey } from './store';
 import { DataSourceListPageEffects } from './store/data-source-list-page';
-import { DataSourceEditorPageEffects } from './store/data-source-editor-page';
 
 @NgModule({
   declarations: [
@@ -23,6 +29,12 @@ import { DataSourceEditorPageEffects } from './store/data-source-editor-page';
     NzTableModule,
     StoreModule.forFeature(dataSourceFeatureStateKey, dataSourceFeatureReducer),
     EffectsModule.forFeature([DataSourceListPageEffects, DataSourceEditorPageEffects]),
+    NzButtonModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzSelectModule,
+    NzCheckboxModule,
   ]
 })
 export class DataSourcesModule {}
--- GoldenReports.UI/src/app/features/data-sources/pages/data-source-editor/data-source-editor.component.ts
+++ GoldenReports.UI/src/app/features/data-sources/pages/data-source-editor/data-source-editor.component.ts
@@ -1,6 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { AppState } from '@core/store';
+import { CreateDataSourceDto, UpdateDataSourceDto } from '@core/api';
+import { DataSourceEditorVm } from '@features/data-sources/models';
+import {
+  dataSourceEditorPageActions,
+  DataSourceEditorPageSelectors
+} from '@features/data-sources/store/data-source-editor-page';
 
 @Component({
   templateUrl: 'data-source-editor.component.html'
 })
-export class DataSourceEditorComponent {}
+export class DataSourceEditorComponent {
+  public vm$!: Observable<DataSourceEditorVm>;
+  public dataSourceForm!: FormGroup;
+
+  constructor(private readonly store: Store<AppState>, private readonly formBuilder: FormBuilder) {
+  }
+
+  public ngOnInit(): void {
+    this.vm$ = this.store.select(DataSourceEditorPageSelectors.getViewModel);
+    this.dataSourceForm = this.createDataSourceForm();
+    this.store.dispatch(dataSourceEditorPageActions.opened());
+  }
+
+  public saveDataSource(dataSourceId: string | undefined, dataSource: CreateDataSourceDto | UpdateDataSourceDto): void {
+    if (dataSourceId) {
+      this.store.dispatch(dataSourceEditorPageActions.changesSubmitted({ dataSourceId, dataSource: dataSource }))
+    } else {
+      this.store.dispatch(dataSourceEditorPageActions.newDataSourceSubmitted({ newDataSource: dataSource }));
+    }
+  }
+
+  private createDataSourceForm(): FormGroup {
+    return this.formBuilder.group({
+      code: [null, [Validators.required, Validators.max(50)]],
+      name: [null, [Validators.required, Validators.max(200)]],
+      description: [null, [Validators.max(750)]],
+      connectionString: [null, [Validators.required, Validators.max(750)]]
+    });
+  }
+}
