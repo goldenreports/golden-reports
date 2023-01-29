a0b0a0b4470e02adab98b9dd1ea06e6b9c925283
Mariano Santoro <santoromariano88@gmail.com>
2023-01-28 19:32:35 -0600

    Replace css with sass

--- GoldenReports.WebUI/ClientApp/src/app/core/layout/components/app-layout/app-layout.component.ts
+++ GoldenReports.WebUI/ClientApp/src/app/core/layout/components/app-layout/app-layout.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: 'app-layout.component.html',
-  styleUrls: ['app-layout.component.css']
+  styleUrls: ['app-layout.component.scss']
 })
 export class AppLayoutComponent {
   isCollapsed = false;
