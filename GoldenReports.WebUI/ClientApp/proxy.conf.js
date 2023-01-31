245721a20cd6f7036ea06e9b9dc7fb9d2c731ec2
Mariano Santoro <santoromariano88@gmail.com>
2023-01-31 17:50:31 -0600

    chore(client): Add favicon to proxy configuration

--- GoldenReports.WebUI/ClientApp/proxy.conf.js
+++ GoldenReports.WebUI/ClientApp/proxy.conf.js
@@ -1,21 +1,20 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7242';
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+  ? env.ASPNETCORE_URLS.split(';')[0]
+  : 'https://localhost:7242';
 
 const PROXY_CONFIG = [
   {
-    context: [
-      "/settings",
-      "/api/",
-      "/silent-refresh"
-   ],
+    context: ['/settings', '/api/', '/silent-refresh.html', '/favicon.ico'],
     target: target,
     secure: false,
     headers: {
-      Connection: 'Keep-Alive'
-    }
-  }
-]
+      Connection: 'Keep-Alive',
+    },
+  },
+];
 
 module.exports = PROXY_CONFIG;
