9d6cfea5f549fab4e8f5ee2d34b2696a3d5d24a4
Mariano Santoro <santoromariano88@gmail.com>
2023-02-06 22:40:21 -0600

    feat: Remove query engine project

--- GoldenReports.QueryEngine/Configuration/EngineSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GoldenReports.QueryEngine.Configuration;
-
-public class EngineSettings
-{
-    public bool EnableAltair { get; set; }
-    
-    public SecuritySettings Security { get; set; }
-}
